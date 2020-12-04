44b72f4275ed24db5f98199caf25c39e9888b1a3
AffanTheBest <61529008+AffanTheBest@users.noreply.github.com>
2020-12-04 15:49:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if(Hls.isSupported())
 {
 var video = document.getElementById('video');
 var hls = new Hls();
-hls.loadSource('https://sonydai.akamaized.net/hls/live/2022316/DAI12ME/master.m3u8?hdnea=exp=1606966095~acl=/*~hmac=fd2d2e63623b39989c07da36de4e73cf62633bc4623793e1aec41a56d222239c');
+hls.loadSource('https://sonydai.akamaized.net/hls/live/2022316/DAI12ME/master.m3u8?hdnea=exp=1607153868~acl=/*~hmac=9d6a57330ac330b7ed4958b278f6e52b3182012b5b92ac0124d2950186cac2be');
 hls.attachMedia(video);
 hls.on(Hls.Events.MANIFEST_PARSED,function()
 {
@@ -37,7 +37,7 @@ hls.on(Hls.Events.MANIFEST_PARSED,function()
 }
 else if (video.canPlayType('application/vnd.apple.mpegurl'))
 {
-video.src = 'https://sonydai.akamaized.net/hls/live/2022316/DAI12ME/master.m3u8?hdnea=exp=1606966095~acl=/*~hmac=fd2d2e63623b39989c07da36de4e73cf62633bc4623793e1aec41a56d222239c';
+video.src = 'https://sonydai.akamaized.net/hls/live/2022316/DAI12ME/master.m3u8?hdnea=exp=1607153868~acl=/*~hmac=9d6a57330ac330b7ed4958b278f6e52b3182012b5b92ac0124d2950186cac2be';
 video.addEventListener('canplay',function()
 {
     video.play();
@@ -48,7 +48,7 @@ if(Hls.isSupported())
 {
 var video1 = document.getElementById('video1');
 var hls = new Hls();
-hls.loadSource('https://sonydai.akamaized.net/hls/live/2022316/DAI12ME/master.m3u8?hdnea=exp=1606966095~acl=/*~hmac=fd2d2e63623b39989c07da36de4e73cf62633bc4623793e1aec41a56d222239c');
+hls.loadSource('https://sonydai.akamaized.net/hls/live/2022315/DAI11ME/master.m3u8?hdnea=exp=1607153777~acl=/*~hmac=531447017322901908aa52d3a833d1a28880cff5ea80f0b0b35b26d073b0cb86');
 hls.attachMedia(video1);
 hls.on(Hls.Events.MANIFEST_PARSED,function()
 {
@@ -57,7 +57,7 @@ hls.on(Hls.Events.MANIFEST_PARSED,function()
 }
 else if (video1.canPlayType('application/vnd.apple.mpegurl'))
 {
-video1.src = 'https://sonydai.akamaized.net/hls/live/2022316/DAI12ME/master.m3u8?hdnea=exp=1606966095~acl=/*~hmac=fd2d2e63623b39989c07da36de4e73cf62633bc4623793e1aec41a56d222239c';
+video1.src = 'https://sonydai.akamaized.net/hls/live/2022315/DAI11ME/master.m3u8?hdnea=exp=1607153777~acl=/*~hmac=531447017322901908aa52d3a833d1a28880cff5ea80f0b0b35b26d073b0cb86';
 video1.addEventListener('canplay',function()
 {
     video1.play();
@@ -68,7 +68,7 @@ if(Hls.isSupported())
 {
 var video2 = document.getElementById('video2');
 var hls = new Hls();
-hls.loadSource('https://sonydai.akamaized.net/hls/live/2022316/DAI12ME/master.m3u8?hdnea=exp=1606966095~acl=/*~hmac=fd2d2e63623b39989c07da36de4e73cf62633bc4623793e1aec41a56d222239c');
+hls.loadSource('https://sonydai.akamaized.net/hls/live/2022317/DAI13ME/master.m3u8?hdnea=exp=1607154038~acl=/*~hmac=bd33b26a1058d60d2368cde62f355346b7832af6945d3d6289aa424dd6214b16');
 hls.attachMedia(video2);
 hls.on(Hls.Events.MANIFEST_PARSED,function()
 {
@@ -77,7 +77,7 @@ hls.on(Hls.Events.MANIFEST_PARSED,function()
 }
 else if (video2.canPlayType('application/vnd.apple.mpegurl'))
 {
-video2.src = 'https://sonydai.akamaized.net/hls/live/2022316/DAI12ME/master.m3u8?hdnea=exp=1606966095~acl=/*~hmac=fd2d2e63623b39989c07da36de4e73cf62633bc4623793e1aec41a56d222239c';
+video2.src = 'https://sonydai.akamaized.net/hls/live/2022317/DAI13ME/master.m3u8?hdnea=exp=1607154038~acl=/*~hmac=bd33b26a1058d60d2368cde62f355346b7832af6945d3d6289aa424dd6214b16';
 video2.addEventListener('canplay',function()
 {
     video2.play();
@@ -96,7 +96,7 @@ function one(){
         video2.pause();
         video.currentTime = video.duration - 4;
         video.play();
-        console.log('Switched to 1080p');
+        console.log('Switched to Hindi');
     }
 }
 
@@ -112,7 +112,7 @@ function seven(){
         video2.pause();
         video1.currentTime = video1.duration - 4;
         video1.play();
-        console.log('Switched to 720p');
+        console.log('Switched to English');
         
     }
 }
@@ -129,7 +129,7 @@ function four(){
         video1.pause();
         video2.currentTime = video2.duration - 4;
         video2.play();
-        console.log('Switched to 480p');
+        console.log('Switched to Tamil');
     }
 }
 
